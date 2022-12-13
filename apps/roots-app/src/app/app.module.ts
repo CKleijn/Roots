9ff88e41fc278ae87c710a294101abf8fd4a4ecb
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-12-13 15:08:05 +0100

    Added nav component

--- apps/roots-app/src/app/app.module.ts
+++ apps/roots-app/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './shared/nav/nav.component';
+import { TimelineComponent } from './pages/timeline/timeline.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent, TimelineComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
