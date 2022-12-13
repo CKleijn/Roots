6eb7f978e38433f00eb94975b280a56697da48ed
KasperVanDenEnden <kaspervde1996@hotmail.com>
2022-12-13 14:01:53 +0100

    company embeds event, add event to company added

--- apps/roots-api/src/app/event/event.module.ts
+++ apps/roots-api/src/app/event/event.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Company, CompanySchema } from '../company/company.schema';
 import { EventController } from './event.controller';
 import { EventSchema } from './event.schema';
 import { EventService } from './event.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }]),
+    MongooseModule.forFeature([
+      { name: Event.name, schema: EventSchema },
+      { name: Company.name, schema: CompanySchema }]),
   ],
   providers: [EventService],
   controllers: [EventController],
--- apps/roots-api/src/app/event/event.service.ts
+++ apps/roots-api/src/app/event/event.service.ts
@@ -1,14 +1,16 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Company, CompanyDocument } from '../company/company.schema';
 import { EventDto } from './event.dto';
 import { EventDocument } from './event.schema';
 
 @Injectable()
 export class EventService {
   constructor(
-    @InjectModel(Event.name) private eventModel: Model<EventDocument>
-  ) { }
+    @InjectModel(Event.name) private eventModel: Model<EventDocument>,
+    @InjectModel(Company.name) private companyModel: Model<CompanyDocument>
+  ) {}
 
   async getAll(): Promise<Event[]> {
     return await this.eventModel.find({});
@@ -18,12 +20,28 @@ export class EventService {
     const event = await this.eventModel.findOne({ _id });
 
     if (!event)
-      throw new HttpException(`This event doesn't exists!`, HttpStatus.NOT_FOUND);
+      throw new HttpException(
+        `This event doesn't exists!`,
+        HttpStatus.NOT_FOUND
+      );
 
     return await event.toObject();
   }
 
-  async create(eventDto: EventDto): Promise<Object> {
-    return await this.eventModel.create(eventDto);
+  async create(eventDto: EventDto, companyId: string): Promise<any> {
+    const event = new this.eventModel(eventDto);
+    const updatedCompanyEvents = await this.companyModel.findOneAndUpdate(
+      { _id: companyId },
+      { $push: { events: event } },
+      { new: true }
+    );
+
+    if (!updatedCompanyEvents)
+      throw new HttpException(
+        `This company (${companyId}) doesn't exist`,
+        HttpStatus.NOT_FOUND
+      );
+
+    return updatedCompanyEvents;
   }
 }
