f94ea350f4a77e08e40c0bfc7c885d24f1192971
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-12-13 17:03:34 +0100

    Added logout button if not authenticated

--- apps/roots-app/src/app/shared/nav/nav.component.ts
+++ apps/roots-app/src/app/shared/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'roots-nav',
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 
 export class NavComponent {
   userAuthenticated!: boolean
+  
+  ngOnInit(): void{
+    
+  }
 }
