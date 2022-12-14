67328837be1c508ef97a27fdca98110d735d41ef
Zuijd <dca.vanzuijdam@student.avans.nl>
2022-12-14 17:50:22 +0100

    added some documentation and added loop to display events

--- apps/roots-app/src/app/app.module.ts
+++ apps/roots-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
@@ -16,6 +16,7 @@ import { TimelineComponent } from './pages/timeline/timeline.component';
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     NgbModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       progressBar: true,
---
+++ apps/roots-app/src/app/event.model.ts
@@ -0,0 +1,7 @@
+export class Event {
+    _id!: string; 
+    title!: string;
+    description!: string;
+    content!: string;
+    eventDate!: Date;
+}  
