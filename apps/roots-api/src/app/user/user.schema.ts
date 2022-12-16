efc22fa4b7211c81e1d9e0e6426498c212adbbf9
GuusvD <g.vandamme@student.avans.nl>
2022-12-16 16:13:23 +0100

    Add unittests for user schema and edited user schema

--- apps/roots-api/src/app/user/user.schema.ts
+++ apps/roots-api/src/app/user/user.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { IUser } from '@roots/data';
-import { IsDefined, IsEmail, IsString, Matches } from 'class-validator';
+import { IsDefined, isEmail, IsEmail, IsString, Matches } from 'class-validator';
 import { ObjectId, Types } from 'mongoose';
 
 export type UserDocument = User & Document;
@@ -9,29 +9,42 @@ export type UserDocument = User & Document;
 export class User implements IUser {
   _id: Types.ObjectId;
 
-  @Prop()
+  @Prop({
+    required: true
+  })
   @IsString({ message: 'Firstname must be a string!' })
   @IsDefined({ message: 'Firstname is required!' })
   firstname: string;
 
-  @Prop()
+  @Prop({
+    required: true
+  })
   @IsString({ message: 'Lastname must be a string!' })
   @IsDefined({ message: 'Lastname is required!' })
   lastname: string;
 
-  @Prop()
   @IsEmail()
   @IsString({ message: 'Email must be a string!' })
   @IsDefined({ message: 'Email is required!' })
+  @Prop({
+    required: true,
+    unique: true,
+    validate: isEmail
+  })
   emailAddress: string;
 
-  @Prop()
+  @Prop({
+    required: true
+  })
   @IsString({ message: 'Password must be a string!' })
   @IsDefined({ message: 'Password is required!' })
   @Matches(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$"),{ message: 'Password not strong enough! Must contain at least: 8 characters, 1 uppercase letter, 1 lowercase letter and 1 number!' })
   password: string;
 
-  @Prop({ref: 'Company'})
+  @Prop({
+    ref: 'Company',
+    type: Types.ObjectId
+  })
   company: ObjectId
 }
 
