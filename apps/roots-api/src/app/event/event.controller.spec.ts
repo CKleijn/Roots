196c4056f19e76b0e58fad35b8c03fcc045f7468
Mike van der Caaij <mvdc2000@hotmail.nl>
2023-01-10 10:36:20 +0100

    Fix tests bugs (eslint related) tag service

--- apps/roots-api/src/app/event/event.controller.spec.ts
+++ apps/roots-api/src/app/event/event.controller.spec.ts
@@ -1,129 +1,161 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { EventService } from "./event.service";
-import { EventController } from "./event.controller";
-import { Event } from './event.schema';
 import { CanActivate } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
 import { Public } from '../auth/auth.module';
+import { EventController } from './event.controller';
+import { EventDto } from './event.dto';
+import { Event } from './event.schema';
+import { EventService } from './event.service';
 
 describe('Event controller - Integration tests', () => {
-    let app: TestingModule;
-    let eventController: EventController;
-    let eventService: EventService;
-    let fakeGuard: CanActivate = { canActivate: () => true };
+  let app: TestingModule;
+  let eventController: EventController;
+  let eventService: EventService;
+  const fakeGuard: CanActivate = { canActivate: () => true };
 
-    beforeAll(async () => {
-        app = await Test.createTestingModule({
-            controllers: [EventController],
-            providers: [{
-                provide: EventService,
-                useValue: {
-                    getAll: jest.fn(),
-                    getById: jest.fn(),
-                    create: jest.fn(),
-                    update: jest.fn(),
-                },
-            }],
-        })
-        .overrideGuard(Public).useValue(fakeGuard)
-        .compile();
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      controllers: [EventController],
+      providers: [
+        {
+          provide: EventService,
+          useValue: {
+            getAll: jest.fn(),
+            getById: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+          },
+        },
+      ],
+    })
+      .overrideGuard(Public)
+      .useValue(fakeGuard)
+      .compile();
 
-        eventController = app.get<EventController>(EventController);
-        eventService = app.get<EventService>(EventService);
-    });
+    eventController = app.get<EventController>(EventController);
+    eventService = app.get<EventService>(EventService);
+  });
 
-    it('should call getAll on the service', async () => {
-        const exampleEvents: Event[] = [
-            {
-                title: 'Event title 1',
-                description: 'Event description 1',
-                content: 'Event content 1',
-                eventDate: new Date()
-            },
-            {
-                title: 'Event title 2',
-                description: 'Event description 2',
-                content: 'Event content 2',
-                eventDate: new Date()
-            }
-        ]
+  it('should call getAll on the service', async () => {
+    const exampleEvents: Event[] = [
+      {
+        title: 'Event title 1',
+        description: 'Event description 1',
+        content: 'Event content 1',
+        eventDate: new Date(),
+        tags: [new Types.ObjectId()],
+      },
+      {
+        title: 'Event title 2',
+        description: 'Event description 2',
+        content: 'Event content 2',
+        eventDate: new Date(),
+        tags: [new Types.ObjectId()],
+      },
+    ];
 
-        const getEvents = jest.spyOn(eventService, 'getAll')
-            .mockImplementation(async () => exampleEvents);
+    const getEvents = jest
+      .spyOn(eventService, 'getAll')
+      .mockImplementation(async () => exampleEvents);
 
-        const results = await eventController.getAllEvents();
+    const results = await eventController.getAllEvents();
 
-        expect(getEvents).toBeCalledTimes(1);
-        expect(results).toHaveLength(2);
-        expect(results[0]).toHaveProperty('title', exampleEvents[0].title);
-        expect(results[0]).toHaveProperty('description', exampleEvents[0].description);
-        expect(results[0]).toHaveProperty('content', exampleEvents[0].content);
-        expect(results[0]).toHaveProperty('eventDate', exampleEvents[0].eventDate);
-        expect(results[1]).toHaveProperty('title', exampleEvents[1].title);
-        expect(results[1]).toHaveProperty('description', exampleEvents[1].description);
-        expect(results[1]).toHaveProperty('content', exampleEvents[1].content);
-        expect(results[1]).toHaveProperty('eventDate', exampleEvents[1].eventDate);
-    });
+    expect(getEvents).toBeCalledTimes(1);
+    expect(results).toHaveLength(2);
+    expect(results[0]).toHaveProperty('title', exampleEvents[0].title);
+    expect(results[0]).toHaveProperty(
+      'description',
+      exampleEvents[0].description
+    );
+    expect(results[0]).toHaveProperty('content', exampleEvents[0].content);
+    expect(results[0]).toHaveProperty('eventDate', exampleEvents[0].eventDate);
+    expect(results[1]).toHaveProperty('title', exampleEvents[1].title);
+    expect(results[1]).toHaveProperty(
+      'description',
+      exampleEvents[1].description
+    );
+    expect(results[1]).toHaveProperty('content', exampleEvents[1].content);
+    expect(results[1]).toHaveProperty('eventDate', exampleEvents[1].eventDate);
+  });
 
-    it('should call getById on the service', async () => {
-        const exampleEvent: Event = {
-            title: 'Event title 1',
-            description: 'Event description 1',
-            content: 'Event content 1',
-            eventDate: new Date()
-        }
+  it('should call getById on the service', async () => {
+    const exampleEvent: Event = {
+      title: 'Event title 1',
+      description: 'Event description 1',
+      content: 'Event content 1',
+      eventDate: new Date(),
+      tags: [new Types.ObjectId()],
+    };
 
-        const getEventById = jest.spyOn(eventService, 'getById')
-            .mockImplementation(async () => exampleEvent);
+    const getEventById = jest
+      .spyOn(eventService, 'getById')
+      .mockImplementation(async () => exampleEvent);
 
-        const eventId = '639a6d184362b5279e5094a0';
+    const eventId = '639a6d184362b5279e5094a0';
 
-        const result = await eventController.getEventById(eventId);
+    const result = await eventController.getEventById(eventId);
 
-        expect(getEventById).toBeCalledTimes(1);
-        expect(result).toHaveProperty('title', exampleEvent.title);
-        expect(result).toHaveProperty('description', exampleEvent.description);
-        expect(result).toHaveProperty('content', exampleEvent.content);
-        expect(result).toHaveProperty('eventDate', exampleEvent.eventDate);
-    });
+    expect(getEventById).toBeCalledTimes(1);
+    expect(result).toHaveProperty('title', exampleEvent.title);
+    expect(result).toHaveProperty('description', exampleEvent.description);
+    expect(result).toHaveProperty('content', exampleEvent.content);
+    expect(result).toHaveProperty('eventDate', exampleEvent.eventDate);
+  });
 
-    it('should call create on the service', async () => {
-        const exampleEvent: Event = {
-            title: 'Event title 1',
-            description: 'Event description 1',
-            content: 'Event content 1',
-            eventDate: new Date()
-        }
+  it('should call create on the service', async () => {
+    const exampleEvent: Event = {
+      title: 'Event title 1',
+      description: 'Event description 1',
+      content: 'Event content 1',
+      eventDate: new Date(),
+      tags: [new Types.ObjectId()],
+    };
 
-        const createEvent = jest.spyOn(eventService, 'create')
-            .mockImplementation(async () => exampleEvent);
+    const createEvent = jest
+      .spyOn(eventService, 'create')
+      .mockImplementation(async () => exampleEvent);
 
-        const companyId = '63988b78e1b33b129a8b04c3';
+    delete exampleEvent.tags;
 
-        const result: any = await eventController.createEvent(companyId, exampleEvent);
+    const eventDto: EventDto = { ...exampleEvent, tags: [] };
 
-        expect(createEvent).toBeCalledTimes(1);
-        expect(result.message).toEqual('De gebeurtenis is succesvol aangemaakt!');
-        expect(result.status).toEqual(201);
-    });
+    const companyId = '63988b78e1b33b129a8b04c3';
 
-    it('should call update on the service', async () => {
-        const exampleEvent: Event = {
-            title: 'Event title 1',
-            description: 'Event description 2',
-            content: 'Event content 1',
-            eventDate: new Date()
-        }
+    const result: any = await eventController.createEvent(companyId, eventDto);
 
-        const updateEvent = jest.spyOn(eventService, 'update')
-            .mockImplementation(async () => exampleEvent);
+    expect(createEvent).toBeCalledTimes(1);
+    expect(result.message).toEqual('De gebeurtenis is succesvol aangemaakt!');
+    expect(result.status).toEqual(201);
+  });
 
-        const companyId = '63988b78e1b33b129a8b04c3';
-        const eventId = '639a6d184362b5279e5094a0';
+  it('should call update on the service', async () => {
+    const exampleEvent: Event = {
+      title: 'Event title 1',
+      description: 'Event description 2',
+      content: 'Event content 1',
+      eventDate: new Date(),
+      tags: [new Types.ObjectId()],
+    };
 
-        const result: any = await eventController.updateEvent(companyId, eventId, exampleEvent);
+    const updateEvent = jest
+      .spyOn(eventService, 'update')
+      .mockImplementation(async () => exampleEvent);
 
-        expect(updateEvent).toBeCalledTimes(1);
-        expect(result.message).toEqual('De gebeurtenis is succesvol aangepast!');
-        expect(result.status).toEqual(200);
-    });
+    delete exampleEvent.tags;
+
+    const eventDto: EventDto = { ...exampleEvent, tags: [] };
+
+    const companyId = '63988b78e1b33b129a8b04c3';
+    const eventId = '639a6d184362b5279e5094a0';
+
+    const result: any = await eventController.updateEvent(
+      companyId,
+      eventId,
+      eventDto
+    );
+
+    expect(updateEvent).toBeCalledTimes(1);
+    expect(result.message).toEqual('De gebeurtenis is succesvol aangepast!');
+    expect(result.status).toEqual(200);
+  });
 });
