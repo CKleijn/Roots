f86260967cc3d2819a7f887ce6032516bcc9d856
Calvin <cl.kleijn@student.avans.nl>
2023-01-16 11:39:28 +0100

    Hotfix select tag filter when filter on terms

--- apps/roots-app/src/app/pages/timeline/filter/filter.model.ts
+++ apps/roots-app/src/app/pages/timeline/filter/filter.model.ts
@@ -1,13 +1,16 @@
 export class Filter {
   radioValue: string | undefined;
   showArchivedEvents: boolean | undefined;
+  searchType: string | undefined;
 
   // Create instance of Filter
   constructor(
     radioValue: string | undefined,
-    showArchivedEvents: boolean | undefined
+    showArchivedEvents: boolean | undefined,
+    searchType: string | undefined
   ) {
     this.radioValue = radioValue;
     this.showArchivedEvents = showArchivedEvents;
+    this.searchType = searchType;
   }
 }
